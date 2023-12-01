d2d60833057568ff46fde2ee5a21634daf83803c
Diego Lara <diegoalara2005@gmail.com>
2023-12-01 14:19:00 -0600

    Tercer commit, F

---
+++ src/lab8p2_diegolara/Parte.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8p2_diegolara;
+
+/**
+ *
+ * @author diego
+ */
+public class Parte {
+    private String marca;
+    private String clasif;
+    private double precio;
+
+    public Parte() {
+    }
+
+    public Parte(String marca, String clasif, double precio) {
+        this.marca = marca;
+        this.clasif = clasif;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getClasif() {
+        return clasif;
+    }
+
+    public void setClasif(String clasif) {
+        this.clasif = clasif;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
